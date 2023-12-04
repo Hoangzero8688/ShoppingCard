6840d75777e68a1f547f79e0b5c772556f85a06c
Hoangzero8688 <140143143+Hoangzero8688@users.noreply.github.com>
2023-12-04 18:29:55 +0700

    Update new 0412

---
+++ ASM_Net104_ShoppingCard/Controllers/NavbarsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASM_Net104_ShoppingCard.Controllers
+{
+	public class NavbarsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+
+		public IActionResult Blog()
+        {
+            return View();
+        }
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+		public IActionResult CheckOut()
+		{
+			return View();
+		}
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
